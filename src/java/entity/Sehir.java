5589733e26c87a137dcf3ec1a7c1292de03ca6b0
FNB06 <74094329+FNB06@users.noreply.github.com>
2020-11-19 22:37:45 +0300

    Update Sehir.java

--- src/java/entity/Sehir.java
+++ src/java/entity/Sehir.java
@@ -10,5 +10,37 @@ package entity;
  * @author Tevhid
  */
 public class Sehir {
+     private Long Sehir_id;
+    private String adi;
+
+    public Sehir() {
+    }
+
+    public Sehir(Long Sehir_id, String adi) {
+        this.Sehir_id = Sehir_id;
+        this.adi = adi;
+    }
+
+    public Long getSehir_id() {
+        return Sehir_id;
+    }
+
+    public void setSehir_id(Long Sehir_id) {
+        this.Sehir_id = Sehir_id;
+    }
+
+    public String getAdi() {
+        return adi;
+    }
+
+    public void setAdi(String adi) {
+        this.adi = adi;
+    }
+
+    @Override
+    public String toString() {
+        return "Sehir{" + "Sehir_id=" + Sehir_id + ", adi=" + adi + '}';
+    }
+    
     
 }
