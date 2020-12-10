ba3006a1c4e9f318b92c5b2a4295759123638b4b
Feyza Nur Budak <74094329+FeyzaNurBudak@users.noreply.github.com>
2020-12-10 23:31:53 +0300

    Update AracController.java

--- src/java/controller/AracController.java
+++ src/java/controller/AracController.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
-
 import dao.AracDAO;
+import dao.DocumentDao;
+import dao.FirmaDAO;
 import entity.Arac;
 import entity.Document;
 import entity.Firma;
@@ -15,23 +11,68 @@ import java.util.List;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
-/**
- *
- * @author Tevhid
- */
 @Named
 @SessionScoped
-public class  AracController implements Serializable{
-     private List<Arac> clist;
+public class AracController implements Serializable {
+
+    private List<Arac> clist;
     private AracDAO adao;
+    private FirmaDAO firmaDAO;
     private List<Firma> firmaList;
+    private DocumentDao documentDao;
     private List<Document> dlist;
+
+    private int page = 1;
+    private int pageSize = 5;
+    private int pageCount;
+
+    public void next() {
+        if (this.page == this.getPageCount()) {
+            this.page = 1;
+        } else {
+            this.page++;
+        }
+    }
+
+    public void previous() {
+        if (this.page == 1) {
+            this.page = this.getPageCount();
+        } else {
+            this.page--;
+        }
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageCount() {
+        this.pageCount = (int) Math.ceil(this.getAdao().count() / (double) pageSize);
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
 
     public AracController() {
     }
-     private Arac arac;
 
-     public void updateForm(Arac arac) {
+    private Arac arac;
+
+    public void updateForm(Arac arac) {
         this.arac = arac;
     }
 
@@ -47,7 +88,7 @@ public class  AracController implements Serializable{
     public void deleteConfirm(Arac arac) {
         this.arac = arac;
     }
-    
+
     public void delete() {
         this.getAdao().delete(this.arac);
         this.arac = new Arac();
@@ -63,8 +104,10 @@ public class  AracController implements Serializable{
         this.getAdao().create(this.arac);
         clearForm();
     }
+
     public List<Arac> getClist() {
-        return clist;
+        this.clist = this.getAdao().read(page, pageSize);
+        return this.clist;
     }
 
     public void setClist(List<Arac> clist) {
@@ -72,6 +115,9 @@ public class  AracController implements Serializable{
     }
 
     public AracDAO getAdao() {
+        if (this.adao == null) {
+            this.adao = new AracDAO();
+        }
         return adao;
     }
 
@@ -79,9 +125,26 @@ public class  AracController implements Serializable{
         this.adao = adao;
     }
 
-  
+    public Arac getArac() {
+        if (this.arac == null) {
+            this.arac = new Arac();
+        }
+        return this.arac;
+    }
+
+    public void setArac(Arac arac) {
+        this.arac = arac;
+    }
+
+    public FirmaDAO getFirmaDAO() {
+        if (firmaDAO == null) {
+            firmaDAO = new FirmaDAO();
+        }
+        return firmaDAO;
+    }
 
     public List<Firma> getFirmaList() {
+        this.firmaList = this.getFirmaDAO().read();
         return firmaList;
     }
 
@@ -89,14 +152,21 @@ public class  AracController implements Serializable{
         this.firmaList = firmaList;
     }
 
-    
+    public DocumentDao getDocumentDao() {
+        if (documentDao == null) {
+            documentDao = new DocumentDao();
+        }
+        return documentDao;
+    }
+
 
     public List<Document> getDlist() {
+        this.dlist = this.getDocumentDao().findAll();
         return dlist;
     }
 
     public void setDlist(List<Document> dlist) {
         this.dlist = dlist;
     }
-    
+
 }
