8e7f1819591662a75243f194678a4217f095b01b
Feyza Nur Budak <74094329+FeyzaNurBudak@users.noreply.github.com>
2020-12-10 23:23:47 +0300

    Update Grup.java

--- src/java/entity/Grup.java
+++ src/java/entity/Grup.java
@@ -1,19 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entity;
 
-/**
- *
- * @author Tevhid
- */
+import java.util.Objects;
+
 public class Grup {
-     private Long grupid;
+
+    private Long grupid;
     private String grupadi;
 
     public Grup() {
+    }
+
+    public Grup(Long grupid, String grupadi) {
+        this.grupid = grupid;
+        this.grupadi = grupadi;
     }
 
     public Long getGrupid() {
@@ -24,6 +23,7 @@ public class Grup {
         this.grupid = grupid;
     }
 
+
     public String getGrupadi() {
         return grupadi;
     }
@@ -32,10 +32,38 @@ public class Grup {
         this.grupadi = grupadi;
     }
 
+    
     @Override
     public String toString() {
         return "Grup{" + "grupid=" + grupid + ", grupadi=" + grupadi + '}';
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.grupid);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Grup other = (Grup) obj;
+        if (!Objects.equals(this.grupid, other.grupid)) {
+            return false;
+        }
+        return true;
+    }
+
     
-    
+
+
 }
