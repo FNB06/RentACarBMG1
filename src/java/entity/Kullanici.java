eb5ce0f6da43f7fc0a6bbb4d67cbf8a73ef78afe
Feyza Nur Budak <74094329+FeyzaNurBudak@users.noreply.github.com>
2020-12-10 23:20:09 +0300

    Update Kullanici.java

--- src/java/entity/Kullanici.java
+++ src/java/entity/Kullanici.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entity;
 
-/**
- *
- * @author Tevhid
- */
+import java.util.Objects;
+
 public class Kullanici {
-     private Long kullaniciid;
+
+    private Long kullaniciid;
     private String email;
     private String kullaniciadi;
     private String sifre;
@@ -19,6 +13,15 @@ public class Kullanici {
     private Grup grup;
 
     public Kullanici() {
+    }
+
+    public Kullanici(Long kullaniciid, String email, String kullaniciadi, String sifre, String telefon, String adres) {
+        this.kullaniciid = kullaniciid;
+        this.email = email;
+        this.kullaniciadi = kullaniciadi;
+        this.sifre = sifre;
+        this.telefon = telefon;
+        this.adres = adres;
     }
 
     public Long getKullaniciid() {
@@ -80,6 +83,31 @@ public class Kullanici {
     @Override
     public String toString() {
         return "Kullanici{" + "kullaniciid=" + kullaniciid + ", email=" + email + ", kullaniciadi=" + kullaniciadi + ", sifre=" + sifre + ", telefon=" + telefon + ", adres=" + adres + ", grup=" + grup + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + Objects.hashCode(this.kullaniciid);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Kullanici other = (Kullanici) obj;
+        if (!Objects.equals(this.kullaniciid, other.kullaniciid)) {
+            return false;
+        }
+        return true;
     }
 
 }
