0150e445644eef3bf192f8ddbcbe249f44cdf8c2
Feyza Nur Budak <74094329+FeyzaNurBudak@users.noreply.github.com>
2020-12-11 14:39:53 +0300

    Update SehirDAO.java

--- src/java/dao/SehirDAO.java
+++ src/java/dao/SehirDAO.java
@@ -7,36 +7,59 @@ package dao;
 
 import entity.Sehir;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- *
- * @author Tevhid
- */
 public class SehirDAO extends Dao {
+
+    public List read(int page, int pageSize) {
+
+        List<Sehir> clist = new ArrayList();
+        int start = (page - 1) * pageSize;
+
+        try {
+            PreparedStatement st = this.getConn().prepareStatement("select * from sehir order by sehir_id limit " +start+ " , " +pageSize);                  
+            ResultSet rs = st.executeQuery();
+            
+            while (rs.next()) {
+                Sehir tmp = new Sehir();
+                tmp.setSehir_id(rs.getLong("sehir_id"));
+                tmp.setAdi(rs.getString("adi"));
+
+                clist.add(tmp);//Her yeni sehiri listeme ekliyorum
+
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return clist;
+    }
+
+    public int count() {
+        int count = 0;
+        
+        try {
+            PreparedStatement st = getConn().prepareStatement("select count(sehir_id) as sehir_count from sehir");
+            ResultSet rs = st.executeQuery();
+            rs.next();
+            count = rs.getInt("sehir_count");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return count;
+    }
 
     @Override
     public void create(Object obj) {
- Sehir sehir = (Sehir) obj;
+        Sehir sehir = (Sehir) obj;
         String q = "insert into sehir(adi) values (?)";
         try {
             PreparedStatement st = getConn().prepareStatement(q);
             st.setString(1, sehir.getAdi());
-
-            st.executeUpdate();
-
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }    }
-
-    @Override
-    public void update(Object obj) {
-         Sehir sehir = (Sehir) obj;
-        String q = "update sehir set adi=? where sehir_id = ?";
-        try {
-            PreparedStatement st = getConn().prepareStatement(q);
-            st.setString(1, sehir.getAdi());
-            st.setLong(2, sehir.getSehir_id());
 
             st.executeUpdate();
 
@@ -58,5 +81,77 @@ public class SehirDAO extends Dao {
             System.out.println(ex.getMessage());
         }
     }
-    
+
+    @Override
+    public void update(Object obj) {
+        Sehir sehir = (Sehir) obj;
+        String q = "update sehir set adi=? where sehir_id = ?";
+        try {
+            PreparedStatement st = getConn().prepareStatement(q);
+            st.setString(1, sehir.getAdi());
+            st.setLong(2, sehir.getSehir_id());
+
+            st.executeUpdate();
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public Sehir find(Long sehir_id) {
+        Sehir s = null;
+        try {
+            PreparedStatement st = this.getConn().prepareStatement("select * from sehir where sehir_id = " + sehir_id);
+            ResultSet rs = st.executeQuery();
+            rs.next();
+
+            s = new Sehir();
+            s.setSehir_id(rs.getLong("sehir_id"));
+            s.setAdi(rs.getString("adi"));
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return s;
+    }
+
+    public List<Sehir> getSehirFirma(Long firmaid) {
+        List<Sehir> firmaSehir = new ArrayList<>();
+        System.out.println(firmaid + "-------------------------------firmaid");
+        try {
+            PreparedStatement st = this.getConn().prepareStatement("select * from firma_sehir where firmaid =" + firmaid);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                firmaSehir.add(this.find(rs.getLong("sehir_id")));
+                System.out.println(this.find(rs.getLong("sehir_id")).toString() + "---------------");
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } 
+        return firmaSehir;
+    }
+
+    public List read() {
+
+        List<Sehir> clist = new ArrayList();
+
+        try {
+            PreparedStatement st = this.getConn().prepareStatement("select * from sehir");
+            ResultSet rs = st.executeQuery();
+
+            while (rs.next()) {
+                Sehir tmp = new Sehir();
+                tmp.setSehir_id(rs.getLong("sehir_id"));
+                tmp.setAdi(rs.getString("adi"));
+
+                clist.add(tmp);
+
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return clist;
+    }
+
 }
