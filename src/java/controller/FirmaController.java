d300b83624ca6cc5d9b6d0b1e26b6ff5e8914fd0
Feyza Nur Budak <74094329+FeyzaNurBudak@users.noreply.github.com>
2020-12-11 14:51:19 +0300

    Update FirmaController.java

--- src/java/controller/FirmaController.java
+++ src/java/controller/FirmaController.java
@@ -1,30 +1,78 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import dao.FirmaDAO;
+import dao.SehirDAO;
 import entity.Firma;
+import entity.Sehir;
 import java.io.Serializable;
 import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 
-/**
- *
- * @author Tevhid
- */
 @Named
 @SessionScoped
 public class FirmaController implements Serializable {
+
+    @Inject
+    private SehirController sehirController;
+
     private List<Firma> clist;
     private FirmaDAO firmaDAO;
+    private List<Sehir> slist;
+    private SehirDAO sdao;
+    private int page = 1;
+    private int pageSize = 5;
+    private int pageCount;
+
+    public void next() {
+        if (this.page == this.getPageCount()) {
+            this.page = 1;
+        } else {
+            this.page++;
+        }
+    }
+
+    public void previous() {
+        if (this.page == 1) {
+            this.page = this.getPageCount();
+        } else {
+            this.page--;
+        }
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageCount() {
+        this.pageCount = (int) Math.ceil(this.getFirmaDAO().count() / (double) pageSize);
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
 
     public FirmaController() {
     }
+
     private Firma firma;
-     public void updateForm(Firma firma) {
-        this.firma = firma;//Guncellemek istedigimiz nesneyi firmacontroller bean nin firma nesnesine atama islemini yaptik.
+
+    public void updateForm(Firma firma) {
+        this.firma = firma;
     }
 
     public void clearForm() {
@@ -56,7 +104,8 @@ public class FirmaController implements Serializable {
     }
 
     public List<Firma> getClist() {
-        return clist;
+        this.clist = this.getFirmaDAO().read(page, pageSize);
+        return this.clist;
     }
 
     public void setClist(List<Firma> clist) {
@@ -64,12 +113,53 @@ public class FirmaController implements Serializable {
     }
 
     public FirmaDAO getFirmaDAO() {
+        if (this.firmaDAO == null) {
+            this.firmaDAO = new FirmaDAO();
+        }
         return firmaDAO;
     }
 
-    public void setFirmaDAO(FirmaDAO firmaDAO) {
+    public void setAdao(FirmaDAO firmaDAO) {
         this.firmaDAO = firmaDAO;
     }
-    
-    
+
+    public Firma getFirma() {
+        if (this.firma == null) {
+            this.firma = new Firma();
+        }
+        return this.firma;
+    }
+
+    public void setFirma(Firma firma) {
+        this.firma = firma;
+    }
+
+    public SehirController getSehirController() {
+        return sehirController;
+    }
+
+    public void setSehirController(SehirController sehirController) {
+        this.sehirController = sehirController;
+    }
+
+    public List<Sehir> getSlist() {
+        return slist;
+    }
+
+    public void setSlist(List<Sehir> slist) {
+        this.slist = slist;
+    }
+
+    public SehirDAO getSdao() {
+        if (this.sdao == null) {
+            this.sdao = new SehirDAO();
+        }
+        return sdao;
+    }
+
+    public List<Firma> getCilist() {
+        this.clist = this.getFirmaDAO().read();
+        return this.clist;
+    }
+
 }
